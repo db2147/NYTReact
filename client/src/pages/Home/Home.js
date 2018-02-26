e1de69d44d3e943cbf1b24d844f6c3b6c0678785
KrisB <david.bierma@gmail.com>
2018-02-26 15:00:27 -0600

    add cors npm

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
 // import Panel from "../../components/Panel";
 // import Article from "../../components/Article";
-import Footer from "../../components/Footer";
 import API from "../../utils/API";
 
 import { Col, Row, Container } from "../../components/Grid";
@@ -104,7 +103,6 @@ class Home extends Component {
             </Form>
           </Col>
         </Row>
-        <Footer />
       </Container>
     );
   }
@@ -112,71 +110,4 @@ class Home extends Component {
 
 export default Home;
 
-// render() {
-//   return (
-//     <Container fluid>
-//       <Row>
-//         <Col size="md-12">
-//           <Jumbotron>
-//             <h1 className="text-center">
-//                   <strong>(ReactJS) New York Times Article Scrubber</strong>
-//                </h1 >
-//             <h2 className="text-center">
-//               Search for and save articles of interest.
-//               </h2>
-//           </Jumbotron>
-//           <form>
-//             <Input
-//               value={this.state.title}
-//               onChange={this.handleInputChange}
-//               name="title"
-//               placeholder="Title (required)"
-//             />
-//             <Input
-//               value={this.state.startyear}
-//               onChange={this.handleInputChange}
-//               name="startyear"
-//               placeholder="Startyear (required)"
-//             />
-//               <Input
-//               value={this.state.endyear}
-//               onChange={this.handleInputChange}
-//               name="endyear"
-//               placeholder="Endyear (required)"
-//             />
-//             <FormBtn
-//               disabled={!(this.state.title && this.state.year)}
-//               onClick={this.handleFormSubmit}
-//             >
-//               Submit Article
-//               </FormBtn>
-//           </form>
-//         </Col>
-{/* <Col size="md-12>
-          <Jumbotron>
-            <h1>Books On My List</h1>
-          </Jumbotron>
-          {this.state.books.length ? (
-            <List>
-              {this.state.books.map(book => (
-                <ListItem key={book._id}>
-                  <Link to={"/articles/" + book._id}>
-                    <strong>
-                      {book.title} by {book.author}
-                    </strong>
-                  </Link>
-                  <DeleteBtn onClick={() => this.deleteBook(book._id)} />
-                </ListItem>
-              ))}
-            </List>
-          ) : (
-              <h3>No Results to Display</h3>
-            )}
-        </Col> */}
-{/* </Row>
-    </Container>
-  );
-}
-} */}
-
-// export default Home;
+ 
